2632d093b96929220d5eee9ad82ce8b8a92b6fa4
YoniTi <yonitiel@gmail.com>
2022-06-09 11:28:56 +0200

    Delete preprocessing.py

--- scripts/preprocessing.py
+++
@@ -1,9 +0,0 @@
-from itertools import islice
-input = snakemake.input
-output = snakemake.output
-		
-with open(input[0], "r") as f, open(output[0], 'w') as outfile:
-		for line in islice(f, 2, None):
-			lineList = line.split()
-			ID = lineList[0]
-			outfile.write(ID+'\n')
